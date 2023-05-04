79882d8df20982e29b049f3f74a6109180124769
lilin <lilinhanl229@foxmail.com>
2023-05-04 15:50:03 +0800

    ..

--- src/dataset.py
+++ src/dataset.py
@@ -1,28 +1,84 @@
+import os
+import random
+
+import numpy as np
 import pandas as pd
-from tqdm import tqdm
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
 
-FILE_NAME = "../dataset/prices-split-adjusted.csv"
+from .config import Config
 
 
-class PriceDataset:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.meta_datas = None
-        self.datas = None
-        self._read()
-        self._split_datas()
+class Data:
+    def __init__(self, config: Config, file_name: str):
+        self.config = config
+        path = self.config.train_data_path + file_name
+        self.data, self.data_column_name = self.read_data(path)
 
-    def _read(self):
-        self.meta_datas = pd.read_csv(self.file_path)
+        self.data_num = self.data.shape[0]
+        self.train_num = int(self.data_num * self.config.train_data_rate)
 
-    def _split_datas(self):
-        keys = self.meta_datas['symbol'].unique().tolist()
-        self.datas = {}
-        for key in tqdm(keys, total=len(keys)):
-            self.datas[key] = self.meta_datas[self.meta_datas['symbol'] == key]
+        self.start_num_in_test = 0  # 测试集中前几天的数据会被删掉，因为它不够一个time_step
 
-    def get_keys(self):
-        return self.datas.keys()
+    def __getitem__(self, index) -> T_co:
+        return
 
-    def get_item(self, key):
-        return self.datas[key]
+    def read_data(self, path):
+        init_data = pd.read_csv(path, usecols=self.config.feature_columns)
+        return init_data.values, init_data.columns.tolist()
+
+    def get_train_data(self):
+        assert self.train_num + self.config.pred_len < self.data_num
+        train_x = [self.data[i:i + self.config.seq_len] for i in range(self.train_num - self.config.seq_len)]
+        train_y = [self.data[i + self.config.seq_len - self.config.label_len:
+                             i + self.config.seq_len + self.config.pred_len]
+                   for i in range(self.train_num - self.config.seq_len)]
+        return np.array(train_x), np.array(train_y)
+
+    def get_valid_data(self):
+        valid_x = [self.data[i:i + self.config.seq_len] for i in
+                   range(self.train_num, self.data_num - self.config.seq_len - self.config.pred_len)]
+        valid_y = [self.data[i + self.config.seq_len - self.config.label_len:
+                             i + self.config.seq_len + self.config.pred_len]
+                   for i in range(self.train_num, self.data_num - self.config.seq_len - self.config.pred_len)]
+        return np.array(valid_x), np.array(valid_y)
+
+    def get_test_data(self):
+        test_x = [self.data[i:i + self.config.seq_len] for i in
+                  range(0, self.data_num - self.config.seq_len - self.config.pred_len)]
+        test_y = [self.data[i + self.config.seq_len - self.config.label_len:
+                            i + self.config.seq_len + self.config.pred_len]
+                  for i in range(0, self.data_num - self.config.seq_len - self.config.pred_len)]
+        return np.array(test_x), np.array(test_y)
+
+
+class StockDataset(Dataset):
+    def __init__(self, config, state="train"):
+        super(StockDataset, self).__init__()
+        self.config = config
+        self.input_datas, self.labels = None, None
+        for file in os.listdir(config.train_data_path):
+            data_provider = Data(config, file)
+            try:
+                i, l = data_provider.get_train_data() if state == "train" else data_provider.get_valid_data()
+                if self.labels is None:
+                    self.input_datas, self.labels = i, l
+                else:
+                    self.input_datas = np.concatenate((self.input_datas, i), axis=0)
+                    self.labels = np.concatenate((self.labels, l), axis=0)
+            except Exception:
+                print(file)
+                continue
+
+    def test_state(self, test_file=None):
+        if test_file is None:
+            files = list(os.listdir(self.config.train_data_path))
+            test_file = random.sample(files, 1)[0]
+        data_provider = Data(self.config, test_file)
+        self.input_datas, self.labels = data_provider.get_test_data()
+
+    def __getitem__(self, index) -> T_co:
+        return self.input_datas[index], self.labels[index]
+
+    def __len__(self):
+        return len(self.input_datas)
--- src/trainer.py
+++ src/trainer.py
@@ -1,30 +1,157 @@
-from src.model import *
+import os
+import time
 import torch
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler, Adam
+from torch.nn import MSELoss
+
+from .dataset import StockDataset
+from .model.ns_transformer import NSTransformer
+from .utils import *
 
 
-def train(x_train, y_train, lr=1e-5, num_epoch=50):
-    model = LSTM4Price()
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device)
-    loss_func = nn.MSELoss().to(device)
-    step = 0
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-    for epoch in range(num_epoch):
+def train(config: Config, logger):
+    train_dataset = StockDataset(config, "train")
+    valid_dataset = StockDataset(Config, "valid")
+    device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu")
 
-        for x, y in zip(x_train, y_train):
-            step += 1
+    train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=config.batch_size, shuffle=True)
+
+    path = os.path.join(config.model_save_path, "")
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    time_now = time.time()
+
+    train_steps = len(train_loader)
+    early_stopping = EarlyStopping(patience=config.patience, verbose=True)
+    model = NSTransformer(config).to(device)
+
+    if config.add_train:
+        model.load_state_dict(torch.load(config.model_save_path + config.model_name))
+    optimizer = Adam(model.parameters(), lr=config.learning_rate)
+    scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=0.1,
+                                      total_iters=int(train_steps * config.train_epochs * 0.8))
+    criterion = MSELoss()
+
+    for epoch in range(config.train_epochs):
+        logger.info("Epoch {}/{}".format(epoch, config.train_epochs))
+        iter_count = 0
+        train_loss = []
+
+        model.train()
+        epoch_time = time.time()
+        for i, (batch_x, batch_y) in enumerate(train_loader):
+            iter_count += 1
             optimizer.zero_grad()
-            x = x.to(device)
-            y = y.to(device)
-            pred_y = model(x)
-            loss = loss_func(pred_y, y)
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float().to(device)
+
+            # decoder input
+            dec_inp = torch.zeros_like(batch_y[:, -config.pred_len:, :]).float()
+            dec_inp = torch.cat([batch_y[:, :config.label_len, :], dec_inp], dim=1).float().to(device)
+
+            # encoder - decoder
+            if config.output_attention:
+                outputs = model(batch_x, dec_inp)[0]
+            else:
+                outputs = model(batch_x, dec_inp)
+            outputs = outputs[:, -config.pred_len:, :]
+            batch_y = batch_y[:, -config.pred_len:, :].to(device)
+            loss = criterion(outputs, batch_y)
+            train_loss.append(loss.item())
+
+            if (i + 1) % 100 == 0:
+                speed = (time.time() - time_now) / iter_count
+                logger.info("\titers: {0}, epoch: {1} | loss: {2:.7f} | speed: {3:.4f}s/iter".format(i + 1, epoch + 1,
+                                                                                                     loss.item(),
+                                                                                                     speed))
+                iter_count = 0
+                time_now = time.time()
+
             loss.backward()
             optimizer.step()
-            if (step + 1) % 50 == 0:
-                print(f"epoch:{epoch},step{step},loss{loss}")
+            scheduler.step()
+
+        logger.info("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+        train_loss = np.average(train_loss)
+        vali_loss = vali(model, valid_loader, criterion, config)
+
+        logger.info("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f}".format(
+            epoch + 1, train_steps, train_loss, vali_loss))
+        early_stopping(vali_loss, model, path, logger)
+        if early_stopping.early_stop:
+            logger.info("Early stopping")
+            break
+
+    best_model_path = path + 'checkpoint.pth'
+    model.load_state_dict(torch.load(best_model_path))
 
     return model
 
 
-def evaluation(model, prediction_days, model_inputs):
-    pass
+def vali(model, vali_loader, criterion, config):
+    total_loss = []
+    device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu")
+    model.eval()
+    with torch.no_grad():
+        for i, (batch_x, batch_y) in enumerate(vali_loader):
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float()
+
+            # decoder input
+            dec_inp = torch.zeros_like(batch_y[:, -config.pred_len:, :]).float()
+            dec_inp = torch.cat([batch_y[:, :config.label_len, :], dec_inp], dim=1).float().to(device)
+            # encoder - decoder
+            if config.output_attention:
+                outputs = model(batch_x, dec_inp)[0]
+            else:
+                outputs = model(batch_x, dec_inp)
+            outputs = outputs[:, -config.pred_len:, :]
+            batch_y = batch_y[:, -config.pred_len:, :].to(device)
+
+            pred = outputs.detach().cpu()
+            true = batch_y.detach().cpu()
+
+            loss = criterion(pred, true)
+
+            total_loss.append(loss)
+    total_loss = np.average(total_loss)
+    model.train()
+    return total_loss
+
+
+def predict(config, test_dataset):
+    test_loader = DataLoader(test_dataset, batch_size=1)
+
+    device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu")
+    model = NSTransformer(config).to(device)
+    model.load_state_dict(torch.load(config.model_save_path + config.model_name))
+
+    result = torch.Tensor().to(device)
+    label = torch.Tensor().to(device)
+
+    model.eval()
+    hidden_predict = None
+    for i, (batch_x, batch_y) in enumerate(test_loader):
+        batch_x = batch_x.float().to(device)
+        batch_y = batch_y.float()
+
+        # decoder input
+        dec_inp = torch.zeros_like(batch_y[:, -config.pred_len:, :]).float()
+        dec_inp = torch.cat([batch_y[:, :config.label_len, :], dec_inp], dim=1).float().to(device)
+        # encoder - decoder
+        if config.output_attention:
+            outputs = model(batch_x, dec_inp)[0]
+        else:
+            outputs = model(batch_x, dec_inp)
+        outputs = outputs[:, -config.pred_len:, :]
+        batch_y = batch_y[:, -config.pred_len:, :].to(device)
+
+        pred = outputs.detach().cpu()
+        true = batch_y.detach().cpu()
+        result = torch.cat((result, pred), dim=0)
+        label = torch.cat((label, true), dim=0)
+
+    return result.detach().cpu().numpy(), label.detach.cpu().numpy()
